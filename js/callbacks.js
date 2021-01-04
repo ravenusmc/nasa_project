ea29cc60e4cb3777531f87b31c213fd1469f8ff0
ravenusmc <mcuddy77@gmail.com>
2021-01-04 18:41:33 -0500

    added current people in space

--- js/callbacks.js
+++ js/callbacks.js
@@ -4,13 +4,13 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Make an AJAX request
-function getJSON(url) {
+function getJSON(url, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = () => {
     if(xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
   xhr.send();
@@ -28,6 +28,13 @@ function generateHTML(data) {
   `;
 }
 
-// getJSON(astrosUrl);
+function getProfiles(json) {
+  json.people.map( person => {
+    getJSON(wikiUrl + person.name, generateHTML);
+  })
+}
 
-btn.addEventListener('click', () => getJSON(astrosUrl));
+btn.addEventListener('click', (event) => {
+  getJSON(astrosUrl, getProfiles);
+  event.target.remove
+});
