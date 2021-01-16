e71561d3721eeac37b49cc3fba50bdbb314cecf4
ravenusmc <mcuddy77@gmail.com>
2021-01-16 11:02:03 -0500

    set up about page

---
+++ client/src/components/generic/AltNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+
+    <!-- BootStrap Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light backgroundColor">
+      <a class="navbar-brand fontColor font" href="#">Space Project</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><router-link class='fontColor font' to="/">Home</router-link></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><router-link class='fontColor font' to="/about">About</router-link></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- End BootStrap Navbar -->
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.backgroundColor {
+  background-color: #041E42 !important;
+}
+.fontColor {
+  color: white !important;
+}
+</style>
---
+++ client/src/components/generic/Footer.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <footer>
+      <div class="footer_fix">
+        <h2 class="footerTitle font">
+          Copyright &copy; 2021 Double Sun Productions<br />
+        </h2>
+        <div>
+          <a href="https://www.linkedin.com/in/mcuddy1/"
+            ><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i
+          ></a>
+          <a href="https://twitter.com/mcuddy7"
+            ><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i
+          ></a>
+          <a href="https://codingsaga.wordpress.com/"
+            ><i class="fa fa-pencil fa-3x" aria-hidden="true"></i
+          ></a>
+          <a href="https://github.com/ravenusmc"
+            ><i class="fa fa-github-square fa-3x" aria-hidden="true"></i
+          ></a>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.fa-linkedin-square,
+.fa-twitter-square,
+.fa-pencil,
+.fa-github-square {
+  color: #bf0d3e;
+  margin: 5px;
+}
+
+footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #041e42;
+  height: 300px;
+  margin-top: 75px;
+}
+
+.footer_fix {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 50px;
+  padding-top: 50px;
+}
+
+.footerTitle {
+  color: #bf0d3e;
+  font-size: 20px;
+  text-transform: uppercase;
+}
+</style>
