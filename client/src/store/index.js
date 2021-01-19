72425a1ad578bba695e0fa0511915940f73e0f60
ravenusmc <mcuddy77@gmail.com>
2021-01-19 18:29:41 -0500

    working on date function

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -19,6 +19,9 @@ export default new Vuex.Store({
   actions: {
 
     fetchMarsData: ({commit}) => {
+      // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY
+      let currentDate = new Date();
+      console.log(currentDate)
       axios
         .get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=' + nasaAPI.nasaAPI)
         .then(response => {
