10bc9c4205b1653ad90e1cd84af31f4ad3368f88
ravenusmc <mcuddy77@gmail.com>
2021-02-14 13:16:17 -0500

    able to display eva data

--- client/src/components/eva/EvaInformation.vue
+++ client/src/components/eva/EvaInformation.vue
@@ -2,22 +2,24 @@
 	<div>
 		<section>
 
-			<div v-for="d of this.vehicleInformation" v-bind:key="d.index">
-				<div class="card" style="width: 18rem;">
-					<div class="card-body">
-						<h3 class="card-title">Vehicle: {{ d.Vehicle }}</h3>
-						<h4 class="card-title">Crew: {{ d.Crew }}</h4>
-						<h5 class="card-title">Country: {{ d.Country }}</h5>
-						<p class="card-text">
-							Duration: {{ d.Duration }}
-							<br/>
-							Purpose: {{ d.Purpose }}
-						</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+			<div class='eva-information-div'>
+				<div class='center-card' v-for="d of this.vehicleInformation" v-bind:key="d.index">
+					<div class="card" style="width: 25rem; height: 400px">
+						<div class="card-body">
+							<h3 class="card-title">Vehicle: {{ d.Vehicle }}</h3>
+							<h4 class="card-title">Crew: {{ d.Crew }}</h4>
+							<h5 class="card-title">Country: {{ d.Country }}</h5>
+							<p class="card-text fix-class-text">
+								Duration: {{ d.Duration }}
+								<br/>
+								Purpose: {{ d.Purpose }}
+							</p>
+							<a href="#" class="btn btn-primary">Go somewhere</a>
+						</div>
 					</div>
 				</div>
 			</div>
-			
+
 		</section>
 	</div>
 </template>
@@ -36,5 +38,24 @@ export default {
 </script>
 
 <style scoped>
+
+.eva-information-div {
+	margin-top: 50px;
+	display: grid; 
+	/* grid-template-columns: 1fr 1fr; */
+	grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+	grid-gap: 2em;
+	justify-content: center;
+}
+
+.center-card {
+	display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.fix-class-text {
+	height: 50%;
+}
 
 </style>
