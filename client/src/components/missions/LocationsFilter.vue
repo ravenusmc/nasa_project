7547d6225eefbd1654207d53077838282b961068
ravenusmc <mcuddy77@gmail.com>
2021-04-03 18:12:40 -0400

    working on locations graph

--- client/src/components/missions/LocationsFilter.vue
+++ client/src/components/missions/LocationsFilter.vue
@@ -1,39 +1,63 @@
 <template>
-	<div>
-		<form @submit="submitSelection">
+  <div>
+    <form @submit="submitSelection">
+      <div class="form-area">
+        <div class="year-area">
+          <p>Year: {{ this.year }}</p>
+        </div>
 
-			<div>
-        <p>Year: {{ this.year }}</p>
+        <div class="button-div">
 
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-        </svg>
+          <div v-on:click='arrowDown()'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              class="bi bi-arrow-down arrow"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+              />
+            </svg>
+          </div>
 
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-        </svg>
+          <div v-on:click='arrowUp()'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              class="bi bi-arrow-up arrow"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+              />
+            </svg>
+          </div>
+        </div>
 
-			</div>
-      
-			<div class='button-div'>
-				<button type="submit" class="btn btn-outline-primary">
-					Submit
-				</button>
-			</div>
-
-      </form>
-	</div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "LocationsFilter",
-	data() {
+  name: "LocationsFilter",
+  data() {
     return {
-			year: 1958,
-		}
+      year: 1958,
+    };
   },
   methods: {
+    arrowDown: function () {
+      console.log('down');
+    },
     submitSelection(evt) {
       console.log("HI");
       evt.preventDefault();
@@ -62,4 +86,26 @@ export default {
 </script>
 
 <style scoped>
+.year-area {
+  font-size: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.form-area {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.button-div {
+  display: flex;
+  flex-direction: row;
+}
+
+.arrow {
+  border: 2px solid black;
+  margin: 10px;
+}
 </style>
